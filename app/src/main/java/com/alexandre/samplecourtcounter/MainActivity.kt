0a019fa6b4a34eb295c12fc13d2940d1344d92b0
Alexandre <alexandre.genet7@gmail.com>
2018-04-25 21:36:40 +0200

    Creating of the layout and the activity without view model

--- app/src/main/java/com/alexandre/samplecourtcounter/MainActivity.kt
+++ app/src/main/java/com/alexandre/samplecourtcounter/MainActivity.kt
@@ -2,11 +2,41 @@ package com.alexandre.samplecourtcounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resetBtn.setOnClickListener{
+            scoreATxt.text = "0"
+            scoreBTxt.text = "0"
+        }
+
+        freeThrowABtn.setOnClickListener{
+            scoreATxt.text = (scoreATxt.text.toString().toInt() + 1).toString()
+        }
+
+        twoPointsABtn.setOnClickListener{
+            scoreATxt.text = (scoreATxt.text.toString().toInt() + 2).toString()
+        }
+
+        threePointsABtn.setOnClickListener{
+            scoreATxt.text = (scoreATxt.text.toString().toInt() + 3).toString()
+        }
+
+        freeThrowBBtn.setOnClickListener{
+            scoreBTxt.text = (scoreATxt.text.toString().toInt() + 1).toString()
+        }
+
+        twoPointsBBtn.setOnClickListener{
+            scoreBTxt.text = (scoreBTxt.text.toString().toInt() + 2).toString()
+        }
+
+        threePointsBBtn.setOnClickListener{
+            scoreBTxt.text = (scoreBTxt.text.toString().toInt() + 3).toString()
+        }
     }
 }
