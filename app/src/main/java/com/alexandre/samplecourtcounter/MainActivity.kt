d277714fa48dea16d1e9cc3f134b2f371777d752
Alexandre <alexandre.genet7@gmail.com>
2018-04-25 22:09:25 +0200

    add ViewModel for orientation changed

--- app/src/main/java/com/alexandre/samplecourtcounter/MainActivity.kt
+++ app/src/main/java/com/alexandre/samplecourtcounter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.alexandre.samplecourtcounter
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,33 +11,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mViewModel = ViewModelProviders.of(this).get(ScoreViewModel::class.java)
+
+        displayScoreTeamA(mViewModel.scoreTeamA)
+        displayScoreTeamB(mViewModel.scoreTeamB)
+
         resetBtn.setOnClickListener{
-            scoreATxt.text = "0"
-            scoreBTxt.text = "0"
+            mViewModel.scoreTeamA = 0
+            mViewModel.scoreTeamB = 0
+            displayScoreTeamA(mViewModel.scoreTeamA)
+            displayScoreTeamB(mViewModel.scoreTeamB)
         }
 
         freeThrowABtn.setOnClickListener{
-            scoreATxt.text = (scoreATxt.text.toString().toInt() + 1).toString()
+            mViewModel.scoreTeamA = mViewModel.scoreTeamA + 1
+            displayScoreTeamA(mViewModel.scoreTeamA)
         }
 
         twoPointsABtn.setOnClickListener{
-            scoreATxt.text = (scoreATxt.text.toString().toInt() + 2).toString()
+            mViewModel.scoreTeamA = mViewModel.scoreTeamA + 2
+            displayScoreTeamA(mViewModel.scoreTeamA)
         }
 
         threePointsABtn.setOnClickListener{
-            scoreATxt.text = (scoreATxt.text.toString().toInt() + 3).toString()
+            mViewModel.scoreTeamA = mViewModel.scoreTeamA + 3
+            displayScoreTeamA(mViewModel.scoreTeamA)
         }
 
         freeThrowBBtn.setOnClickListener{
-            scoreBTxt.text = (scoreATxt.text.toString().toInt() + 1).toString()
+            mViewModel.scoreTeamB = mViewModel.scoreTeamB + 1
+            displayScoreTeamB(mViewModel.scoreTeamB)
         }
 
         twoPointsBBtn.setOnClickListener{
-            scoreBTxt.text = (scoreBTxt.text.toString().toInt() + 2).toString()
+            mViewModel.scoreTeamB = mViewModel.scoreTeamB + 2
+            displayScoreTeamB(mViewModel.scoreTeamB)
         }
 
         threePointsBBtn.setOnClickListener{
-            scoreBTxt.text = (scoreBTxt.text.toString().toInt() + 3).toString()
+            mViewModel.scoreTeamB = mViewModel.scoreTeamB + 3
+            displayScoreTeamB(mViewModel.scoreTeamB)
         }
+    }
+
+    fun displayScoreTeamA(scoreTeamA: Int) {
+        scoreATxt.text = scoreTeamA.toString()
+    }
+
+    fun displayScoreTeamB(scoreTeamB: Int) {
+        scoreBTxt.text = scoreTeamB.toString()
     }
 }
